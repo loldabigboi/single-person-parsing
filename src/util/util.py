e574bad4d59fe0e99fe8299ecbcec5f4b63e6684
loldabigboi <loldabigboi@gmail.com>
2020-11-11 18:23:37 +1300

    Refined code directory structure and added image input preprocessing helper method

---
+++ src/util/util.py
@@ -0,0 +1,11 @@
+import cv2
+
+def preprocess(images, req_dims=(224,224)):
+    '''
+        Performs necessary preprocessing to make the images in 'images' ready for input to the model. The default dims of
+        req_dims=224x224 are retrieved from https://tfhub.dev/tensorflow/resnet_50/classification/1, as this is the model which
+        provides feature maps to the single person parsing model.
+    '''
+
+    return [cv2.resize(img/255.0, req_dims) for img in images]
+
